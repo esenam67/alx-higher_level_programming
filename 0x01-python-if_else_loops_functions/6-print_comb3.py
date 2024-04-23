01637e33babaf6d9835f65aa46c5fa6551193959
esenam67 <moneyesenam@gmail.com>
2024-04-23 10:57:25 +0000

    my commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for combination in range(0, 10):
+    for combination2 in range(combination + 1, 10):
+        if combination == 8 and combination2 == 9:
+            print("{}{}".format(combination, combination2))
+        else:
+            print("{}{}".format(combination, combination2), end=", ")
+print()
