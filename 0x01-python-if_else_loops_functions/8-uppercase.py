ae4f174c48c3403c753082eef2d6093db9ddb2d2
esenam67 <moneyesenam@gmail.com>
2024-04-29 18:03:25 +0000

    my commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    """ prints string in capital letter"""
+    for x in str:
+        if x >= 'a' and x <= 'z':
+            x = ord(x) - 32
+            print(x)
+        else:
+            print(x)
