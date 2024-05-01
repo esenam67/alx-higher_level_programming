28a0b292e565e93bdb31fb31d44172f76dd0ae1d
esenam67 <moneyesenam@gmail.com>
2024-05-01 20:00:27 +0000

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
-if count == 1:
-    print("{} argument:".format(count))
-elif count == 0:
-    print("{} arguments.".format(count))
-else:
-    print("{} arguments:".format(count))
-for i in range(1, count + 1):
-    print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("{} argument:".format(count))
+    elif count == 0:
+        print("{} arguments.".format(count))
+    else:
+        print("{} arguments:".format(count))
+    for i in range(1, count + 1):
+        print(f"{i}: {sys.argv[i]}")
