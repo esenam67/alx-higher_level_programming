187be0447e0c6fa7a1bd259b132e729ab53d52bc
esenam67 <moneyesenam@gmail.com>
2024-05-01 08:09:29 +0000

    my commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 #calculator_1
+""" function does and print some maths"""
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
+
 if __name__ == "__main__":
-    """ function does and print some maths"""
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    pass
